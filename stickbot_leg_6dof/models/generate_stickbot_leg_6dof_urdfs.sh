31c99831bc662ddff6898339c8e965ef6cb6e5d5
Chien-Liang Fok <liangfok@utexas.edu>
2015-06-05 12:30:09 -0500

    Added rviz.launch to stickbot_leg_6dof.

--- stickbot_leg_6dof/models/generate_stickbot_leg_6dof_urdfs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo Creating directory \'urdf\'
-mkdir -p stickbot_leg_6dof/urdf
-
-echo Generating stickbot_leg_6dof/urdf/stickbot_leg_6dof.urdf
-rosrun xacro xacro.py stickbot_leg_6dof/xacro/stickbot_leg_6dof.xacro -o stickbot_leg_6dof/urdf/stickbot_leg_6dof.urdf
-
-echo Done!
