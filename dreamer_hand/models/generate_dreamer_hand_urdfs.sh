a470adde1a1f5ff5dd36133481bce94557806a07
Chien-Liang Fok <liangfok@utexas.edu>
2015-06-05 15:27:54 -0500

    Added rviz.launch to atlas_legs_plain, atlas_plain, atlas_plain_pinned, dreamer, dreamer_hand, dreamer_testbed, stickbot_humanoid_32dof_pinned.

--- dreamer_hand/models/generate_dreamer_hand_urdfs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo Creating directory \'urdf\'
-mkdir -p dreamer_hand/urdf
-
-echo Generating dreamer_hand/urdf/dreamer_hand.urdf
-rosrun xacro xacro.py dreamer_hand/xacro/dreamer_hand.xacro -o dreamer_hand/urdf/dreamer_hand.urdf
-
-echo Done!
--- stickbot_humanoid_32dof_pinned/models/generate_stickbot_humanoid_32dof_pinned_urdfs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo Creating directory \'urdf\'
-mkdir -p stickbot_humanoid_32dof_pinned/urdf
-
-echo Generating stickbot_humanoid_32dof_pinned/urdf/stickbot_humanoid_32dof_pinned.urdf
-rosrun xacro xacro.py stickbot_humanoid_32dof_pinned/xacro/stickbot_humanoid_32dof_pinned.xacro -o stickbot_humanoid_32dof_pinned/urdf/stickbot_humanoid_32dof_pinned.urdf
-
-echo Done!
