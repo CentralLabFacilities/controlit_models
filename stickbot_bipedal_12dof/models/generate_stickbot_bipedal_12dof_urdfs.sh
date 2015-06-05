0166f09e38e4605308582eb5a9e1cf8f839c2ff7
Chien-Liang Fok <liangfok@utexas.edu>
2015-06-05 12:32:14 -0500

    Added rviz.launch to stickbot_bipedal_12dof.

--- stickbot_bipedal_12dof/models/generate_stickbot_bipedal_12dof_urdfs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo Creating directory \'urdf\'
-mkdir -p stickbot_bipedal_12dof/urdf
-
-echo Generating stickbot_bipedal_12dof/urdf/stickbot_bipedal_12dof.urdf
-rosrun xacro xacro.py stickbot_bipedal_12dof/xacro/stickbot_bipedal_12dof.xacro -o stickbot_bipedal_12dof/urdf/stickbot_bipedal_12dof.urdf
-
-echo Done!
