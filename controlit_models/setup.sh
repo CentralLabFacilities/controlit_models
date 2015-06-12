77be47297d3319b626cb3cafe015ac2db8359649
Chien-Liang Fok <liangfok@utexas.edu>
2015-06-12 14:50:12 -0500

    Removed gazebo plugins from controlit_models.

--- controlit_models/setup.sh
+++ controlit_models/setup.sh
@@ -3,5 +3,5 @@ config_directories=`find $controlit_models_dir/.. -mindepth 1 -maxdepth 1 -type 
 
 for d in $config_directories; do
     export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$d/models
-    export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$d/lib
+    # export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$d/lib
 done
