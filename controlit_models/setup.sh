ea37a852be15bbd63146e3e41f33f769f3e40f95
Chien-Liang Fok <liangfok@utexas.edu>
2015-07-16 11:35:56 -0500

    Modified controlit_models/setup.sh to only include directories with models that are needed by Gazebo.

--- controlit_models/setup.sh
+++ controlit_models/setup.sh
@@ -1,7 +1,11 @@
 controlit_models_dir=`rospack find controlit_models`
-sub_directories=`find $controlit_models_dir/.. -mindepth 1 -maxdepth 1 -type d \( ! -iname ".git" \)`
 
-for d in $sub_directories; do
-    export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$d/models
-    # export GAZEBO_PLUGIN_PATH=$GAZEBO_PLUGIN_PATH:$d/lib
+# Only include directories that contain models needed by Gazebo
+sub_directories=(dreamer controlit_models_common)
+#sub_directories=`find $controlit_models_dir/.. -mindepth 1 -maxdepth 1 -type d \( ! -iname ".git" \)`
+
+for d in ${sub_directories[@]}; do
+    current_path="$controlit_models_dir/../$d/models"
+	# echo "Adding to GAZEBO_MODEL_PATH: $current_path"
+    export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$current_path
 done
